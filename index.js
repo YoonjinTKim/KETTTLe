a510017344e508fa5057f549f8ae7a30493f6dbf
Tyler Thompson <tylert0@vt.edu>
2018-02-08 15:38:56 -0500

    browser testing for submmitting credentials

--- index.js
+++ index.js
@@ -4,5 +4,24 @@ var mongojs = require('mongojs');
 var db = mongojs(process.env.MONGO_URL || 'mongodb://localhost:27017/ketttle-db');
 
 var app = express();
-app.listen(3000, () => console.log('listening on *:3000'));
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "user-create.htm");
+})
+
+app.get('/process_get', function (req, res) {
+    response = {
+        first_name:req.query.first_name,
+        last_name:req.query.last_name,
+        email:req.query.email,
+        password:req.query.password
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
