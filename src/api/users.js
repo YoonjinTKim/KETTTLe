2c4e72d0976f02e350f3f843a5c4cb337dee3db2
Tyler Thompson <tylert0@vt.edu>
2018-02-15 10:28:03 -0500

    users added to data base

--- src/api/users.js
+++ src/api/users.js
@@ -15,13 +15,24 @@ module.exports = {
         db.users.find({email: req.body.email} , (err, result) => {
             console.log(result);
             res.sent(result);
-            if result.length == 0 {
+            if (result.length || result.length == 0) {
                 //insert this user into the database
+                db.users.insert({
+                    email: req.body.email,
+                    first_name: req.body.first_name,
+                    last_name: req.body.last_name,
+                    //might want to salt and hash the password eventually which
+                    //would require us to also store the salt for each user
+                    //BUT for now, just reverse the password
+                    password: req.body.password.split('').reverse().join('')
+
+                })
             }
             else {
                 //tell the user that the email slready exists
+                res.sent('That email is already associated with an account, please enter another email.');
             }
-        }
+            });
         console.log(response);
 
         //res.end(JSON.stringify(response));
