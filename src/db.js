bb0a5cd667d2eb53cbde4e83d862ed9c16f7f136
Vincent Le <vinnyoodles@gmail.com>
2018-02-26 18:23:31 -0500

    Add db indices

--- src/db.js
+++ src/db.js
@@ -1,3 +1,17 @@
 var mongojs = require('mongojs');
 var db = mongojs(process.env.MONGO_URL || 'mongodb://localhost:27017/ketttle-db');
+
+db.jobs.createIndex({
+    user_id: 1
+});
+
+db.jobs.createIndex({
+    user_id: 1,
+    updated_at: 1
+});
+
+db.jobs.createIndex({
+    status: 1
+})
+
 module.exports = db;
