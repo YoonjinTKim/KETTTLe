c1ce0d6e9078c5aeb502360556d22c2f0ae13bc1
Vincent Le <vinnyoodles@gmail.com>
2018-03-17 13:06:20 -0400

    Add try catch for db indices

--- src/db.js
+++ src/db.js
@@ -1,17 +1,22 @@
-var mongojs = require('mongojs');
-var db = mongojs(process.env.MONGO_URL || 'mongodb://localhost:27017/ketttle-db');
+const mongojs = require('mongojs');
+const db = mongojs(process.env.MONGO_URL || 'mongodb://localhost:27017/ketttle-db');
+const logger = require('./logger');
 
-db.jobs.createIndex({
-    user_id: 1
-});
+try {
+    db.jobs.createIndex({
+        user_id: 1
+    });
 
-db.jobs.createIndex({
-    user_id: 1,
-    updated_at: 1
-});
+    db.jobs.createIndex({
+        user_id: 1,
+        updated_at: 1
+    });
 
-db.jobs.createIndex({
-    status: 1
-})
+    db.jobs.createIndex({
+        status: 1
+    })
+} catch(err) {
+    logger.log({ level: 'error', message: err.message });
+}
 
 module.exports = db;
